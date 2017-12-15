b9e461b3f5abfa0a7c8817b66ffd1bbfb51c9d8a
CodinCafe <codincafe@gmail.com>
2017-12-15 22:00:42 +0530

    Initial commit

---
+++ ntwice-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# NTwice
+# http://codincafe.com
+
+#
+# Currently Supported Operating Systems:
+#
+#   RHEL 5, RHEL 6
+#   CentOS 5, CentOS 6
+#   Debian 7
+#   Ubuntu LTS, Ubuntu 13.04, Ubuntu 13.10
+#
+
+# Am I root?
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
